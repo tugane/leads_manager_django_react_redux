e8b7609af7aacdaf116f587dd333973bde3ecfd9
tugane <tooamiel@gmail.com>
2021-09-02 23:24:42 +0200

    initail commit

---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,9 @@
+from django.db import router
+from rest_framework import routers, urlpatterns
+from .api import LeadViewSet
+
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewSet, 'leads')
+
+urlpatterns = router.urls
